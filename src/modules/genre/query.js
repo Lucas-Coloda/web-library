949bb8f3f8a2104a5ab6d8fabad768002050826c
Lucas Coloda <lg.coloda@gmail.com>
2021-05-22 16:43:27 -0300

    Remover items de teste babel

--- src/modules/genre/query.js
+++ src/modules/genre/query.js
@@ -1,7 +1,5 @@
 import FakeDB from '../../../db/fakeDB/index.js'
 
-import FakeDB2 from 'db/fakeDB/index.js'
-
 const getGenre = (_, { id }) => FakeDB.genre.genres.find(genre => genre.id == id)
 const getGenres = () => FakeDB.genre.genres
 
